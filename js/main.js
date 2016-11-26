7868c3af811e5a616449a3b58c237e0ab09fa9f8
holmgr <viktor.holmgren@gmail.com>
2016-11-26 12:08:43 +0100

    Create basic canvas and function to get all pixel values

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+
+// Create canvas containing the test image
+let canvas = document.createElement('canvas');
+let context = canvas.getContext('2d');
+let img = document.getElementById('main-image');
+context.drawImage(img, 0, 0);
+
+let pixels = getPixels(img, context);
+
+// Print 10 first pixels
+console.log(pixels.slice(0, 10));
+
+// Returns an array of pixels, RGBA
+function getPixels(image, context) {
+  let pixels = [];
+
+  for (var offsetX = 0; offsetX < image.width; offsetX++) {
+    for (var offsetY = 0; offsetY < image.height; offsetY++) {
+      pixels.push(context.getImageData(offsetX, offsetY, 1, 1));
+    }
+  }
+  return pixels;
+}
