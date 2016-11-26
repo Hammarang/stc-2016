ff05a38ab7caef67cfb8a13d380ed6d6847dfa03
holmgr <viktor.holmgren@gmail.com>
2016-11-26 17:25:01 +0100

    Implement contrast, brightness and saturation functions

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,11 @@
 let canvas = document.createElement('canvas');
 let context = canvas.getContext('2d');
 let img = document.getElementById('main-image');
-context.drawImage(img, 0, 0);
+
+
+console.log(img);
+
+//context.drawImage(img, 0, 0);
 let pixels = getPixels(img, context);
 
 let colorThief = new ColorThief();
@@ -24,6 +28,55 @@ let intensity = getPaletteIntensity(dominantPalette)
 
 console.log("Intensity of dominant palette")
 console.log(intensity);
+
+console.log("Constrast of dominant palette");
+let contrast = getPaletteContrast(dominantPalette);
+console.log(contrast);
+
+console.log("Brightness of image");
+let brightness = getBrightness(dominantPalette);
+console.log(brightness);
+
+console.log("Saturation of image");
+let saturation = getSaturation(dominantPalette);
+console.log(saturation);
+
+// Calculates "contrast" in relation to most dominant color
+function getPaletteContrast(palette) {
+  let lightnessPalette = palette.map(color => {
+    return rgbToHsl(color).l
+  });
+  let mainColor = lightnessPalette[0];
+  let squareLightDiff = 0;
+  for (var i = 1; i < lightnessPalette.length; i++) {
+    squareLightDiff += Math.pow(lightnessPalette[i] - mainColor, 2);
+  }
+  return Math.pow(squareLightDiff/lightnessPalette.length, 0.5);
+}
+
+// Calculates the average brightness of the palette
+function getBrightness(palette) {
+  return palette
+    .map(color => {
+      return rgbToHsl(color).l
+    })
+    .reduce((sum, val) => {
+      return sum + val
+    })
+  /palette.length
+}
+
+// Calculates the average saturation of the palette
+function getSaturation(palette) {
+  return palette
+    .map(color => {
+      return rgbToHsl(color).s
+    })
+    .reduce((sum, val) => {
+      return sum + val
+    })
+  /palette.length
+}
 
 // Gets the intensity of a palette of colors. The intensity being the V-values
 // in HSV form
@@ -48,6 +101,18 @@ function rgbToHsv(pixel) {
     }
   ).toHsv();
 }
+
+// Convert an array of RGB values to HSV
+function rgbToHsl(pixel) {
+  return tinycolor(
+    {
+      r: pixel[0],
+      b: pixel[1],
+      g: pixel[2],
+    }
+  ).toHsl();
+}
+
 
 // Returns an array of pixels, RGBA
 function getPixels(image, context) {
